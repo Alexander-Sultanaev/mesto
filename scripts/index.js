85dec60f153844039828a7192dcd2be7d70598cd
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2022-05-12 14:40:58 +0500

    fix after validation

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,3 @@
 const openButton = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__button-close');
-console.log(openButton);
-console.log(popup);
-console.log(closeButton);
