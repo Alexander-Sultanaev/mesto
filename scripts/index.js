4f7fa3e5cb43b9305855ec63deedaa26ea08e881
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2022-05-12 12:22:51 +0500

    mobile verst

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const openButton = document.querySelector('.profile__button-edit');
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__button-close');
+console.log(openButton);
+console.log(popup);
+console.log(closeButton);
