07d7e8498b8a6bd4f9797fd22675ab95fdf8da97
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2022-05-12 15:33:27 +0500

    betta version

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,24 @@
 const openButton = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__button-close');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = popup.querySelector('.popup__input_type_name');
+const subnameInput = popup.querySelector('.popup__input_type_subname');
+const profName = document.querySelector('.profile__name');
+const profSubname = document.querySelector('.profile__subname');
+const toglePopup = function (event) {
+  popup.classList.toggle("popup_opened")
+};
+openButton.addEventListener('click', toglePopup);
+closeButton.addEventListener('click', toglePopup);
+popup.addEventListener('click', function(event){
+  if (event.target === event.currentTarget)
+  toglePopup();
+});
+function formSubmitHandler (event) {
+  event.preventDefault();
+  profName.textContent = nameInput.value;
+  profSubname.textContent = subnameInput.value;
+  toglePopup();
+};
+formElement.addEventListener('submit', formSubmitHandler); 
