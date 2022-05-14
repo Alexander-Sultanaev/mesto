45b616df1eb30b6b4891bcf5f1653ee06bae3c48
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2022-05-14 15:32:35 +0500

    fix-one

--- scripts/index.js
+++ scripts/index.js
@@ -6,19 +6,26 @@ const nameInput = popup.querySelector('.popup__input_type_name');
 const subnameInput = popup.querySelector('.popup__input_type_subname');
 const profName = document.querySelector('.profile__name');
 const profSubname = document.querySelector('.profile__subname');
-const toglePopup = function (event) {
+const popupContainer = popup.querySelector('.popup__container');
+
+const toglePopup = function(event) {
   popup.classList.toggle("popup_opened")
+  nameInput.value = profName.textContent;
+  subnameInput.value = profSubname.textContent;
 };
-openButton.addEventListener('click', toglePopup);
-closeButton.addEventListener('click', toglePopup);
-popup.addEventListener('click', function(event){
-  if (event.target === event.currentTarget)
-  toglePopup();
-});
+
+/*popup.addEventListener('click', function(event) {
+  if(event.target === event.currentTarget)
+    toglePopup();
+}); нашел только способ через добовлние блока overlay на всю ширину экрана!(не знаю на сколько это правильно )*/
+
 function formSubmitHandler (event) {
   event.preventDefault();
   profName.textContent = nameInput.value;
   profSubname.textContent = subnameInput.value;
   toglePopup();
 };
+
+openButton.addEventListener('click', toglePopup);
+closeButton.addEventListener('click', toglePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
