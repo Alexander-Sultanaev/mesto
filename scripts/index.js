67c02f7de09f5afd7c9cb8d8a9929986ec5cc6d6
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2022-05-27 08:56:47 +0500

    Добавили карточки через js и новый попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,44 @@
 const openButtonPofile = document.querySelector('.profile__button-edit');
-const openButtonCard = document.querySelector('.profile__button-add');
+const profName = document.querySelector('.profile__name');
+const profSubname = document.querySelector('.profile__subname');
 
 const popup = document.querySelector('.popup');
 const popupContainer = document.querySelector('.popup__container');
 
 //popupCard
-const popupCard = document.querySelector('.popup_type_card');
-const closeButtonCard = document.querySelector('.popup__button-close_card');
-const formElementCard = document.querySelector('.popup__form_type_card');
-const cardTitle = document.querySelector('.popup__input_type_title');
-const cardSrc = document.querySelector('.popup__input_type_src');
+
 //popupProfile
 const popupProfile = document.querySelector('.popup_type_profile');
-const closeButtonProfile = document.querySelector('.popup__button-close_profile');
-const formElementProfile = document.querySelector('.popup__form_type_profile');
-const nameInput = document.querySelector('.popup__input_type_name');
-const subnameInput = document.querySelector('.popup__input_type_subname');
-const profName = document.querySelector('.profile__name');
-const profSubname = document.querySelector('.profile__subname');
-//////1231241244
+const closeButtonProfile = popupProfile.querySelector('.popup__button-close_profile');
+const formElementProfile = popupProfile.querySelector('.popup__form_type_profile');
+const nameInput = popupProfile.querySelector('.popup__input_type_name');
+const subnameInput = popupProfile.querySelector('.popup__input_type_subname');
 
+
+//4Проектная работа
+function openPopupProfile(event) {
+  popupProfile.classList.add("popup_opened")
+  nameInput.value = profName.textContent;
+  subnameInput.value = profSubname.textContent;
+};
+
+function closePopupProfile (event) {
+  popupProfile.classList.remove("popup_opened")
+};
+
+function formSubmitHandlerProfile (event) {
+  event.preventDefault();
+  profName.textContent = nameInput.value;
+  profSubname.textContent = subnameInput.value;
+  closePopupProfile();
+};
+
+openButtonPofile.addEventListener('click', openPopupProfile);
+closeButtonProfile.addEventListener('click', closePopupProfile);
+formElementProfile.addEventListener('submit', formSubmitHandlerProfile);
+//Конец 4 работы.
+
+//Отображение карточек на странице
 const initialCards = [
   {
     name: 'Архыз',
@@ -46,45 +65,57 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-console.log(initialCards);
-//popupProfile
-function openPopupProfile(event) {
-  popupProfile.classList.toggle("popup_opened")
-  nameInput.value = profName.textContent;
-  subnameInput.value = profSubname.textContent;
-};
 
-function closePopupProfile (event) {
-  popupProfile.classList.remove("popup_opened")
-};
 
-function formSubmitHandlerProfile (event) {
-  event.preventDefault();
-  profName.textContent = nameInput.value;
-  profSubname.textContent = subnameInput.value;
-  openPopupProfile();
-};
-//popupCard
-function openPopupCard(event) {
-  popupCard.classList.toggle("popup_opened")
-};
 
+const openButtonCard = document.querySelector('.profile__button-add');
+const cardsList = document.querySelector('.gallery__list');
+const popupCard = document.querySelector('.popup_type_card');
+const closeButtonCard = popupCard.querySelector('.popup__button-close_card');
+const formButtonSubmit = document.querySelector('.popup__form_type_card');
+//открываем попап
+function openPopupCard (event) {
+  popupCard.classList.add("popup_opened");
+};
+//закрываем попап
 function closePopupCard (event) {
-  popupCard.classList.remove("popup_opened")
+  popupCard.classList.remove("popup_opened");
 };
 
+///////////////// создаем карточки через js
+const elementTemplate = document.querySelector('.gallery__template').content;
 
-function formSubmitHandlerCard (event) {
+initialCards.forEach((element) => {
+  cardsList.prepend(createElement(element));
+});
+
+function createElement (element) {
+  const newElement = elementTemplate.querySelector('.gallery__card').cloneNode(true);
+  const elementTitle = newElement.querySelector('.gallery__title');
+  elementTitle.textContent = element.name;
+  const elementImage = newElement.querySelector('.gallery__image');
+  elementImage.src = element.link;
+  elementImage.alt = element.name;
+  return newElement;
+};
+
+//кнопака лайка
+
+
+//добавляем картоки
+function newElementSubmit(event) {
   event.preventDefault();
-  let name = cardTitle.value
-  let link = cardSrc.value
-  openPopupCard();
+  const cardNameInput = document.querySelector('.popup__input_type_title');
+  const cardLinkInput = popupCard.querySelector('.popup__input_type_link'); 
+  const newCard = { name: cardNameInput.value, link: cardLinkInput.value };
+  cardsList.prepend(createElement(newCard));
+  cardNameInput.value = null;
+  cardLinkInput.value = null;
 };
 
-openButtonPofile.addEventListener('click', openPopupProfile);
-closeButtonProfile.addEventListener('click', closePopupProfile);
-formElementProfile.addEventListener('submit', formSubmitHandlerProfile);
+
 
 openButtonCard.addEventListener('click', openPopupCard);
 closeButtonCard.addEventListener('click', closePopupCard);
-formElementCard.addEventListener('submit', formSubmitHandlerCard);
+formButtonSubmit.addEventListener('submit', newElementSubmit);
+// отображение карточек на странице
