a553879bae72a69bfe0e9b4532c0cddf0cbda601
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2022-07-12 11:20:04 +0500

    oop refactor

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,11 @@
+import initialCards from "./initial-cards.js";
+import { Card } from "./card.js";
+import { FormValidator } from "./formValidator.js";
+
+
 const openButtonPofile = document.querySelector('.profile__button-edit');
 const openButtonCard = document.querySelector('.profile__button-add');
 const closeButtonPopup = document.querySelectorAll('.popup__button-close');
-
-const popup = document.querySelector('.popup');
-const popupContainer = document.querySelector('.popup__container');
 
 const profName = document.querySelector('.profile__name');
 const profSubname = document.querySelector('.profile__subname');
@@ -15,111 +17,83 @@ const subnameInput = popupProfile.querySelector('.popup__input_type_subname');
 const cardsList = document.querySelector('.gallery__list');
 const popupCard = document.querySelector('.popup_type_card');
 const formButtonSubmit = document.querySelector('.popup__form_type_card');
-const popupImage = document.querySelector('.popup_type_image');
 
-const elementTemplate = document.querySelector('.gallery__template').content;
 const cardNameInput = popupCard.querySelector('.popup__input_type_title');
 const cardLinkInput = popupCard.querySelector('.popup__input_type_link');
 
-const popupAddPlace = Array.from(popupCard.querySelectorAll(".popup__input"));
-const popupButtonSave = popupCard.querySelector(".popup__button-save");
+//добавление карточек в разметку-----------------
+function createCards(item) {
+  const card = new Card(item, ".gallery__template");
+  const cardElement = card.generateCard();
+  cardsList.prepend(cardElement);
+};
+initialCards.forEach((element) => {
+  createCards(element);
+});
+//валидация форм-------------
+const validationSettings  = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button-save",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_active",
+};
+
+const addProfileValidator = new FormValidator(validationSettings, popupCard);
+addProfileValidator.enableValidation();
+
+const editProfileValidator = new FormValidator(validationSettings, popupProfile);
+editProfileValidator.enableValidation();
+
+//работа с попапами-------------------------
 //открытие попапа
-function openPopup(popup) {
+export function openPopup(popup) {
   popup.addEventListener("mousedown", closePopupWithOverlay);
   document.addEventListener("keydown", closePopupWithEscape);
   popup.classList.add("popup_opened");
 };
-
 //попап профиля
 function openPopupProfile() {
   nameInput.value = profName.textContent;
   subnameInput.value = profSubname.textContent;
-  deleteErrors(popupProfile, setup);
+  editProfileValidator.deleteInputError();
   openPopup(popupProfile);
 };
-
 //открытие попапа карточки
 function openPopupCard() {
   cardNameInput.value = null;
   cardLinkInput.value = null;
-  deleteErrors(popupCard, setup);
+  addProfileValidator.deleteInputError();
   openPopup(popupCard);
 };
-
 //закрытие всех попапов 
 closeButtonPopup.forEach((button) => {
   const popup = button.closest(".popup");
   button.addEventListener("click", () => closePopup(popup));
 });
-
-
 //Функция удаления класса открытия попапа
 function closePopup(popup) {
   popup.removeEventListener("mousedown", closePopupWithOverlay);
   document.removeEventListener("keydown", closePopupWithEscape);
   popup.classList.remove("popup_opened");
 };
-
 function closeOpenedPopup() {
   const openedPopup = document.querySelector(".popup_opened");
   closePopup(openedPopup);
 };
-
 //Функция закрытия попапов кликом на оверлей
 function closePopupWithOverlay(evt) {
   if (evt.target === evt.currentTarget) {
     closeOpenedPopup();
   }
 };
-
 //Функция закрытия попапов на клавишу Esc
 function closePopupWithEscape(evt) {
   if (evt.key === "Escape") {
     closeOpenedPopup();
   }
 };
-
-//отображение карточек
-initialCards.forEach((element) => {
-  cardsList.prepend(createElement(element));
-});
-
-function createElement(element) {
-  const newElement = elementTemplate.querySelector('.gallery__card').cloneNode(true);
-  const elementTitle = newElement.querySelector('.gallery__title');
-  elementTitle.textContent = element.name;
-  const elementImage = newElement.querySelector('.gallery__image');
-  elementImage.src = element.link;
-  elementImage.alt = element.name;
-  const buttonLike = newElement.querySelector('.gallery__button-like');
-  buttonLike.addEventListener('click', likeElement);
-  const buttonDelete = newElement.querySelector('.gallery__button-delete');
-  buttonDelete.addEventListener('click', deleteElement);
-  const openImage = newElement.querySelector('.gallery__image');
-  openImage.addEventListener('click', openPopupImage);
-  return newElement;
-};
-
-//кнопака лайка
-function likeElement(event) {
-  event.target.classList.toggle("gallery__button-like_active");
-};
-
-//кнопка дэлит
-function deleteElement(event) {
-  event.target.closest('.gallery__card').remove();
-};
-
-//открытия картинки
-function openPopupImage(event) {
-  const increasedImage = popupImage.querySelector('.popup__image');
-  increasedImage.src = event.target.src;
-  increasedImage.alt = event.target.alt;
-  const increasedElementCaption = popupImage.querySelector('.popup__caption');
-  increasedElementCaption.textContent = event.target.alt;
-  openPopup(popupImage);
-};
-
 //отпроаление данных на страницу
 function formSubmitHandlerProfile(event) {
   event.preventDefault();
@@ -132,12 +106,11 @@ function formSubmitHandlerProfile(event) {
 function newElementSubmit(event) {
   event.preventDefault();
   const newCard = { name: cardNameInput.value, link: cardLinkInput.value };
-  cardsList.prepend(createElement(newCard));
+  createCards(newCard);
   closePopup(popupCard);
 };
 
-
-//слушатели событий
+//слушатели событий ------------------------------
 openButtonCard.addEventListener('click',openPopupCard);
 openButtonPofile.addEventListener('click', openPopupProfile);
 formButtonSubmit.addEventListener('submit', newElementSubmit);
